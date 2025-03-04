e3c967524e698fbe97698f89987afb2a94d8b954
Carles Mateu <carlesm@carlesm.com>
2025-03-04 12:21:18 +0100

    Added video snippets to server

--- src/xarxes2025/server.py
+++ src/xarxes2025/server.py
@@ -1,4 +1,7 @@
 from loguru import logger
+from udpdatagram import UDPDatagram
+from videoprocessor import VideoProcessor
+
 
 class Server(object):
     def __init__(self, port):       
@@ -11,3 +14,25 @@ class Server(object):
         logger.debug(f"Server created ")
         
     
+
+    # # 
+    # # This is not complete code, it's just an skeleton to help you get started.
+    # # You will need to use these snippets to do the code.
+    # # 
+    # #     
+    # def send_udp_frame(self):
+      
+    #     # This snippet reads from self.video (a VideoProcessor object) and prepares 
+    #     # the frame to be sent over UDP. 
+
+    #     data = self.video.next_frame()
+    #     if data:
+    #         if len(data)>0:
+    #                 frame_number = self.get_frame_number()
+    #                 # create UDP Datagram
+
+    #                 udp_datagram = UDPDatagram(frame_number, data).get_datagram()
+
+    #                 # send UDP Datagram
+    #                 socketudp.sendto(udp_datagram, (address, port))
+                        
