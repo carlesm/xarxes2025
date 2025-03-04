a99c5387076581fc02154447b2d4783b2a4124e7
Carles Mateu <carlesm@carlesm.com>
2025-03-04 11:58:43 +0100

    Added initial skeleton

---
+++ src/xarxes2025/server.py
@@ -0,0 +1,13 @@
+from loguru import logger
+
+class Server(object):
+    def __init__(self, port):       
+        """
+        Initialize a new VideoStreaming server.
+
+        :param port: The port to listen on.
+        """
+
+        logger.debug(f"Server created ")
+        
+    
